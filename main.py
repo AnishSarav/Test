1bdf6db696d16a8fdf361633fa9d2917ba0491cc
Anish <anishvar.saravanan@gmail.com>
2025-03-29 17:37:15 -0400

    Push to dev 101 - Subtract and Multiplication

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def addnumbers(a,b):
     return a+b
 
+def subtractnumbers(a,b):
+    return a-b
+
+def multiplynumbers(a,b):
+    return a*b
 
 print("The sum is:", addnumbers(1,2))
+print("The sum is:", subtractnumbers(1,2))
+print("The sum is:", multiplynumbers(1,2))
 
 
 
