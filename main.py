1ad158ac9e7aebab08a6c9e2b3ff4f1d65187f6b
Anish <anishvar.saravanan@gmail.com>
2025-03-29 17:43:05 -0400

    chang

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def multiplynumbers(a,b):
     return a*b
 
 print("The sum is:", addnumbers(1,2))
-print("The sum is:", subtractnumbers(1,2))
+print("The difference is:", subtractnumbers(1,2))
 print("The sum is:", multiplynumbers(1,2))
 
 
